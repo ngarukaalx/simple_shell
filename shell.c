c6fa9b1e218add903db5ccd1065f98e6774b74c0
ngarukaalx <hiramkabuagi@gmail.com>
2023-07-26 11:11:49 -0700

    gateline fuction

--- shell.c
+++ shell.c
@@ -7,43 +7,12 @@
 */
 int main(int argc, char **argv)
 {
-	char *line = NULL;
-	size_t str_len = 0;
-	int is_interactivemode, status, tokencount = 0, x, y, cmdnum = 1;
+	int is_interactivemode;
 
 	is_interactivemode = isatty(STDIN_FILENO);
 	if (is_interactivemode == 0 && argc == 1)
 	{
-		while ((y = getline(&line, &str_len, stdin)) > 0)
-		{
-			tokencount = _count_token(line);
-			status = handle_line(line, tokencount, argv, cmdnum);
-			line = NULL;
-			if (status != 0)
-			{
-				free(line);
-				exit(status);
-			}
-		}
-		free(line);
-		return (status);
+		return (non_interactive_mode(argv));
 	}
-	while (is_interactivemode)
-	{
-		write(1, "$ ", 2);
-		tokencount = 0;
-		x = getline(&line, &str_len, stdin);
-		if (x < 0)
-		{
-			free(line);
-			write(1, "\n", 1);
-			break;
-		}
-		tokencount = _count_token(line);
-		handle_line(line, tokencount, argv, cmdnum);
-		cmdnum++;
-		line = NULL;
-		free(line);
-	}
-	return (0);
+	return (interactive_mode(argv));
 }
