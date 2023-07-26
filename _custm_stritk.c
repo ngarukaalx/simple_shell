435b91f533625a2e760180e7ca9c1fb7073c7a46
nickfff-dev <hunchographics@gmail.com>
2023-07-26 22:56:10 +0300

    strtok betty

--- _custm_stritk.c
+++ _custm_stritk.c
@@ -1,27 +1,31 @@
 #include "main.h"
+
+/**
+  * my_stritk - the function name
+  * @str: parameter of type char *.
+  * @delim: parameter of type const char *.
+  * Return: char *.
+ */
 char *my_stritk(char *str, const char *delim)
 {
-    static char *src = NULL;
-    char  *p,  *ret = 0;
+	static char *src;
+	char *p, *ret = 0;
 
-    if (str != NULL)
-        src = str;
-
-    if (src == NULL)
-        return NULL;
-
-    if ((p = strpbrk(src, delim)) != NULL) 
-    {
-        *p  = 0;
-        ret = src;
-        src = ++p;
-
-    }
-    else if (*src)
-    {
-        ret = src;
-        src = NULL;
-    }
-
-    return ret;
+	if (str != NULL)
+	{
+		src = str;
+	}
+	p = strpbrk(src, delim);
+	if (p != NULL)
+	{
+		*p  = 0;
+		ret = src;
+		src = ++p;
+	}
+	else if (*src)
+	{
+		ret = src;
+		src = NULL;
+	}
+	return (ret);
 }
