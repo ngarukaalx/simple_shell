125bd3721f771e637fdc756f645884f2a4beefdc
ngarukaalx <hiramkabuagi@gmail.com>
2023-07-27 17:28:15 -0700

    betty done

--- _custm_stritk.c
+++ _custm_stritk.c
@@ -1,31 +1,37 @@
 #include "main.h"
 
 /**
-  * my_stritk - the function name
-  * @str: parameter of type char *.
-  * @delim: parameter of type const char *.
-  * Return: char *.
+ * my_stritk - fuction name
+ * @str:pointer to char*
+ * @delim:pointer to char*
+ *
+ * Return:char
  */
+
 char *my_stritk(char *str, const char *delim)
 {
 	static char *src;
-	char *p, *ret = 0;
+	char  *p,  *ret = 0;
 
 	if (str != NULL)
-	{
 		src = str;
-	}
+
+	if (src == NULL)
+		return (NULL);
+
 	p = strpbrk(src, delim);
 	if (p != NULL)
 	{
 		*p  = 0;
 		ret = src;
 		src = ++p;
+
 	}
 	else if (*src)
 	{
 		ret = src;
 		src = NULL;
 	}
+
 	return (ret);
 }
